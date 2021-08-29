6ba7325ef4df49f90dd2b2034624486bfd67ed94
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-08-29 12:16:57 -0300

    exercício concluido OlaUsuario

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeUsuario;
+            Console.Write("Digite seu nome: ");
+            nomeUsuario = Console.ReadLine();  
+
+            Console.WriteLine($"Olá, {nomeUsuario}!");
+
+        }
+    }
+}
